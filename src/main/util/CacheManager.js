13bc8fcfef7e64b5ea4a3384acf3a127d4d8e7dd
Foulest <43710301+Foulest@users.noreply.github.com>
2026-02-08 16:28:36 -0800

    Froze providers list

--- src/main/util/CacheManager.js
+++ src/main/util/CacheManager.js
@@ -51,7 +51,7 @@ const CacheManager = (() => {
         expirationTime = Number.isFinite(expSeconds) && expSeconds >= min ? expSeconds : def;
     });
 
-    const providers = [
+    const providers = Object.freeze([
         // Global
         "global",
 
@@ -69,7 +69,7 @@ const CacheManager = (() => {
         "seclookup",
         "switchCH",
         "quad9",
-    ];
+    ]);
 
     // Initialize caches for each provider
     for (const name of providers) {
